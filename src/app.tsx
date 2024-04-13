2aabd6072eeac30b3901ebbc0937892e2c171929
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-04-13 23:56:36 +0100

    ...

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="grid min-h-screen grid-cols-app">
       <Sidebar />
-      <main className="px-8 pb-12 pt-8">
+      <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:p-8">
         <Home />
       </main>
     </div>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,6 +5,7 @@ import {
   Flag,
   Home,
   LifeBuoy,
+  Menu,
   Search,
   SquareStack,
   Users,
@@ -15,11 +16,20 @@ import { Logo } from './Logo'
 import { UsedSpaceWIdget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
 import * as Input from '../Input'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Button } from '../button'
 
 export function Sidebar() {
   return (
-    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
+    <Collapsible.Root className="fixed bottom-0 left-0 right-0 top-0 z-20 flex h-screen flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
+      <div className="flex items-center justify-between ">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="size-6" />
+          </Button>
+        </Collapsible.Trigger>
+      </div>
       <Input.Root>
         <Input.Prefix>
           <Search className=" h-5 w-5 text-zinc-500" />
@@ -44,6 +54,6 @@ export function Sidebar() {
         <div className="h-px bg-zinc-200" />
         <Profile />
       </div>
-    </aside>
+    </Collapsible.Root>
   )
 }
